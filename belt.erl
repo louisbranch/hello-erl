8f4128116b9edd10be0410cdd731d313fc51ea83
Luiz Branco <artfikan@gmail.com>
2012-08-09 00:14:11 -0300

    Fix factorial function

--- belt.erl
+++ belt.erl
@@ -19,8 +19,8 @@ fib(1) -> 1;
 fib(N) -> fib(N-2) + fib(N-1).
 
 % Factorial
-fac(1) -> 1;
-fac(N) -> N * fac(N-1).
+fac(0) -> 1;
+fac(N) when N > 0 -> N * fac(N-1).
 
 % Average of a list
 avg([]) -> 0;
